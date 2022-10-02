af7325d88bac813c3583649304792b7f613f2d06
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2022-10-03 00:08:51 +0200

    exercise 5

--- ponyracer/src/app/app.module.ts
+++ ponyracer/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { RacesComponent } from './races/races.component';
 import { MenuComponent } from './menu/menu.component';
 import { RaceComponent } from './race/race.component';
+import { PonyComponent } from './pony/pony.component';
 
 @NgModule({
-  declarations: [AppComponent, RacesComponent, MenuComponent, RaceComponent],
+  declarations: [AppComponent, RacesComponent, MenuComponent, RaceComponent, PonyComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ponyracer/src/app/pony/pony.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PonyModel } from '../models/pony.model';
+
+@Component({
+  selector: 'pr-pony',
+  templateUrl: './pony.component.html',
+  styleUrls: ['./pony.component.css']
+})
+export class PonyComponent {
+  @Input() ponyModel!: PonyModel;
+
+  constructor() {}
+
+  getPonyImageUrl(): string {
+    return `assets/images/pony-${this.ponyModel.color}.gif`.toLowerCase();
+  }
+
+  @Output() readonly ponyClicked = new EventEmitter<PonyModel>();
+
+  clicked(): void {
+    this.ponyClicked.emit(this.ponyModel);
+  }
+}
