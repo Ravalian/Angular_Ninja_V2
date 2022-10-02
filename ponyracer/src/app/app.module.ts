68f4ea0a436db9338168ade4bdff1a0862e771af
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2022-10-02 23:22:58 +0200

    4 exercise

--- ponyracer/src/app/app.module.ts
+++ ponyracer/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RacesComponent } from './races/races.component';
 import { MenuComponent } from './menu/menu.component';
+import { RaceComponent } from './race/race.component';
 
 @NgModule({
-  declarations: [AppComponent, RacesComponent, MenuComponent],
+  declarations: [AppComponent, RacesComponent, MenuComponent, RaceComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
